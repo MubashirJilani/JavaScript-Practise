e1d4019d84e0183045e2d691e34324e630d76c3c
Mubashir Jilani <mubashirjilani0@gmail.com>
2025-08-02 00:48:34 +0500

    Done

--- Class-01/index.js
+++ Class-01/index.js
@@ -1,10 +1,42 @@
-// alert("Hello")
+alert("Hello")
 
-// alert("Welcome")
+alert("Welcome")
 
 var myName = ("Hello World")
-// alert(myName)
+alert(myName)
 
 var myName = ("Mubashir Jelani")
 
 alert(myName)
+
+
+var Price = 45;
+var Tax = 10;
+
+var total = Price + Tax;
+
+alert(total)
+
+var monthlySalary = 37000;
+var monthInYear = 12;
+
+var yearlyIncome = monthlySalary * monthInYear;
+
+alert("Yearly Income is " +yearlyIncome)
+
+var numOfStudents = 140;
+var numOfTeachers = 4;
+
+var studentsOfEachTeacher = numOfStudents / numOfTeachers;
+
+alert(studentsOfEachTeacher)
+ 
+
+var calculateNum = 5 + 9 - 3 * 1;
+alert(calculateNum)
+
+var groupedCalculation = (5 + 9 - 7 + 7) * 9
+alert(groupedCalculation)
+
+var percentCalculate = 5 % 7
+alert(percentCalculate)
