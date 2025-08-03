be7c2c55439e7724899ee62c2827bc9a68524347
Mubashir Jilani <mubashirjilani0@gmail.com>
2025-08-04 00:00:24 +0500

    Done

--- Class-02/index.js
+++ Class-02/index.js
@@ -1,3 +1,72 @@
 alert("12 + 7 is = " + (12 + 7) );
 alert(100 - 25);
 alert(8 * 6);
+
+
+
+
+
+
+var x = 12;
+var y = 13;
+var z = x + y;
+alert(z);
+
+var schoolFee = 5000;
+var totalChild = 3;
+var totalCost = schoolFee * totalChild;
+alert(totalCost);
+
+var scamPerPerson = 18000;
+var totalScam = 12;
+var totalScamEarning = scamPerPerson * totalScam;
+alert(totalScamEarning);
+
+var totalMan = 7;
+var totalScamEarning = 216000;
+var myPercentInScam = totalScamEarning / totalMan;
+alert("My total earning in scam is " + myPercentInScam.toFixed(0)); 
+
+
+
+
+
+
+var myName = "Mubashir";
+var myAge = 19;
+
+var myNumber = 923184772520;
+var myEmail = "mubashirjilani0@gmail.com"
+
+var myArea = 35 + "/D";
+console.log(myArea);
+
+// var 1user = "mubashir"
+// console.log(1user);
+
+// var @house = "mubashir"
+// console.log(@house);
+
+
+
+
+
+
+
+var studentName = "Mubashir"
+var studentId = 310245;
+var studentGrade = "A1"
+var obtainedMarks = 467;
+var totalMarks = 490;
+
+alert("Name of student is " + studentName +
+    "\n Roll No is " + studentId + 
+    "\n Obtained Marks " + obtainedMarks + 
+    "\n Total Marks " + totalMarks + 
+    "\n Grade " + studentGrade 
+)
+
+
+
+
+
